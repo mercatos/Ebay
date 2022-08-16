6ed32f26ab3ba831965f2bb30b130dbd80df894d
ThakurKarthik <iamkarthik08@gmail.com>
2022-08-16 21:04:19 +0530

    fixed missing details of product and created clientconfig file
    - Now we need to set the client_id and client_secret as environment variables to get the token
    - Generate client_token locally via base64
    - Give sensible default value types for missing data

--- ebay-apis/clientCredentials.py
+++ ebay-apis/clientCredentials.py
@@ -1,30 +1,48 @@
 import requests
-import datetime
+# import datetime
+import base64
+from clientConfig import is_dev_environment, clientConfig
 
-sandbox_url = 'https://api.sandbox.ebay.com/identity/v1/oauth2/token'
-prod_url = 'https://api.ebay.com/identity/v1/oauth2/token'
-
-expiry_time = None
+# expiry_time = None
 resp_json = None
+
 def fetchCredentials():
-  global expiry_time
-  #check if last fetch is still having valid token
-  #Enable this when running on server
-  # if expiry_time and (datetime.datetime.now() < expiry_time):
-  #   print("Using Previous fetched access token")
-  #   return resp_json
+  # global expiry_time
+  global resp_json
   
-  headers = {
-    'Content-Type': 'application/x-www-form-urlencoded', 
-    'Authorization': 'Basic QWR2ZXJ0TWUtZGVhbHMtU0JYLTdiMWFjNDEwMi1jYzQ4YjM0MDpTQlgtYjFhYzQxMDJmNzVhLTA5NjUtNGIyZC1hOGQ0LTY5MWQ=' #this is a sandbox token
-  }
-  request_params = {
-    'grant_type': 'client_credentials',
-    'scope': 'https://api.ebay.com/oauth/api_scope'
-  }
-  resp = requests.post(sandbox_url, data=request_params, headers=headers)
-  # resp_json is of the form {access_token, expires_in, token_type}
-  resp_json = resp.json()
-  expiry_time = datetime.datetime.now() + datetime.timedelta(0, int(resp_json['expires_in']))
-  # print(resp.text, resp.headers, resp.json())
+  try:
+    #check if last fetch is still having valid token
+    #Enable this when running on server
+    # if expiry_time and (datetime.datetime.now() < expiry_time):
+    #   print("Using Previous fetched access token")
+    #   return resp_json
+    client_config = clientConfig.get("production " if not is_dev_environment else "sandbox")
+    client_id = client_config.get("client_id", "")
+    client_secret = client_config.get("client_secret", "")
+    api_endpoint = client_config.get("token_url", "")
+    client_token = base64.b64encode(f"{client_id}:{client_secret}".encode('ascii')).decode("ascii")
+
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Authorization': f'Basic {client_token}' #this is a sandbox token
+    }
+    request_params = {
+      'grant_type': 'client_credentials',
+      'scope': 'https://api.ebay.com/oauth/api_scope'
+    }
+
+    print("Fetching access token")
+    
+    resp = requests.post(api_endpoint, data=request_params, headers=headers)
+    
+    # resp_json is of the form {access_token, expires_in, token_type}
+    resp_json = resp.json()
+    # expiry_time = datetime.datetime.now() + datetime.timedelta(0, int(resp_json['expires_in']))
+    print("Finshed fetching access token")
+
+  except Exception as e:
+    import traceback
+    print("Failed to fetch access token", e)
+    print("Error stack trace", traceback.format_exc())
+  
   return resp_json
