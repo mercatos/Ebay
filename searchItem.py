75f26697f9807430c299affe749ae5ff94dbd2f5
MadhuVaddi114 <madhu.cryptodeveloper@gmail.com>
2022-08-17 20:32:39 +0530

    epid added

--- searchItem.py
+++ searchItem.py
@@ -1,3 +1,5 @@
+from distutils.log import error
+import warnings
 import requests
 import getCredentials
 import utils
@@ -5,23 +7,28 @@ import utils
 prod_url_search = 'https://api.ebay.com/buy/browse/v1/item/'
 sandbox_url_search = 'https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search'
 
-def getItemDetails(search_string, limit=15, offset=0, condition='New', buyingOption='FIXED_PRICE', deliveryCountry='US'):
+def getItemDetails(search_string, limit=15, offset=0, condition='New', buyingOption='FIXED_PRICE', deliveryCountry='US', epid=None):
   credentials = getCredentials.fetchCredentials()
   headers = {
     'Authorization': f"Bearer {credentials['access_token']}"
   }
   fc = utils.getConditionId(condition)
+  params = {
+    'q': search_string, 
+    'limit': limit, 
+    'offset': offset, 
+    'filter': 'conditionIds:{'+fc+'},buyingOptions:{'+buyingOption+'},deliveryCountry:'+deliveryCountry
+  }
+  if epid:
+    params['epid'] = epid
   response = requests.get(f"{sandbox_url_search}", 
-    params={
-      'q': search_string, 
-      'limit': limit, 
-      'offset': offset, 
-      'filter': 'conditionIds:{'+fc+'}&buyingOption:{'+buyingOption+'}&deliveryCountry:'+deliveryCountry
-    },
+    params=params,
     headers=headers
   )
   response_json = response.json()
+
   required_data = []
+  errors = []
   if 'itemSummaries' in response_json:
     for item in response_json['itemSummaries']:
       required_data.append({
@@ -32,9 +39,14 @@ def getItemDetails(search_string, limit=15, offset=0, condition='New', buyingOpt
         'condition': item['condition'],
         'URL': item['itemWebUrl']
       })
-
-    return required_data
-
+    return required_data, None
+    
+  elif 'warnings' in response_json:
+    for warn in response_json['warnings']:
+      errors.append(warn['message'])
+    return None, errors
+  else:
+    return [], None
 
 # --- Buying Options ---
 # FIXED_PRICE
@@ -42,5 +54,5 @@ def getItemDetails(search_string, limit=15, offset=0, condition='New', buyingOpt
 # BEST_OFFER
 # CLASSIFIED_AD
 
-print(getItemDetails('laptop', 15, 0, 'Used|Excellent - Refurbished', 'FIXED_PRICE', 'US'))
-    
+items, errors = getItemDetails('laptop', 15, 0, 'Used|Excellent - Refurbished', 'FIXED_PRICE', 'US')
+print(items, errors)
